8fcd06d0bac62feb7ed1854fa66fea14a8774121
Julia Cortez <jcgaucha@gmail.com>
2023-05-16 15:39:19 -0300

    inicial test

--- code/weather_prevision.py
+++ code/weather_prevision.py
@@ -1,6 +1,32 @@
-# link do open_weather: https://openweathermap.org/
+"""
+the Requests library in Python serves to make requests on a database or dataset. 
+Thus, it is essential to establish the interaction between a database and an application in Python.
+Get - is used to get information.
+Post - to create a source information
+Patch - is indicated to update information
+Delete - is for deleting information.
 
+
+wttr is a console-oriented weather forecast service that comes with a number of information representation 
+ways to make sure you get the weather data in the best form possible.
+"""
 import requests
-from pprint import pprint
 
-#api_key=
+
+
+# Function to Generate Report
+def city_function(C):
+    url = f'https://wttr.in/{C}'
+    try:
+        received_url = requests.get(url)
+        result = received_url.text
+    except:
+        result = "Falha ao localizar essa cidade!"
+    print(result)
+    
+
+
+city = input("Digite o nome da cidade que você deseja ver a previsão do tempo: ")
+print("\n\n")
+
+city_function(city)
